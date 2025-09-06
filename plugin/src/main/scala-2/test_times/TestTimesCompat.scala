2a68f063a8ff852083ac784e780e6773cbd4893c
xuwei-k <6b656e6a69@gmail.com>
2025-09-06 17:26:50 +0900

    update hasScalaTestDependency task

--- plugin/src/main/scala-2/test_times/TestTimesCompat.scala
+++ plugin/src/main/scala-2/test_times/TestTimesCompat.scala
@@ -7,8 +7,8 @@ import sbt.Keys.target
 private[test_times] trait TestTimesCompat { self: TestTimesPlugin.type =>
   import self.autoImport.*
 
-  val testExternalDependencyClasspathValue: Def.Initialize[Task[Seq[File]]] = Def.task {
-    (Test / externalDependencyClasspath).value.map(_.data)
+  val testExternalDependencyClasspathValue: Def.Initialize[Task[Seq[ModuleID]]] = Def.task {
+    (Test / externalDependencyClasspath).value.flatMap(_.get(Keys.moduleID.key))
   }
 
   implicit class DefOps(self: Def.type) {
